82aaf1a8bd569102525fff7b7302f95561f3cd08
liangjiajun <liangjiajun@yaeher.com>
2020-07-17 14:24:29 +0800

    feat：添加主界面，信息绑定界面，预约界面

---
+++ src/pages/Appointment/appointment.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="appointment-page">
+    <div class="header">预约送水</div>
+    <van-form @submit="onFormSubmit" class="form">
+      <van-field
+        class="form-item"
+        label-class="form-label"
+        v-model="formData.username"
+        name="username"
+        label="姓名"
+        placeholder="请输入姓名"
+      />
+      <van-field
+        class="form-item"
+        label-class="form-label"
+        v-model="formData.phone"
+        name="phone"
+        label="手机号码"
+        placeholder="请输入姓名"
+      />
+      <van-button type="primary" block class="submit-btn">提交</van-button>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import {
+  Form as VanForm,
+  Field as VanField,
+  Button as VanButton,
+} from "vant";
+export default {
+  components: {
+    VanForm,
+    VanField,
+    VanButton,
+  },
+  data() {
+    return {
+      formData: {
+        username: "",
+        area: "",
+        phone: "",
+        buildingNum: "",
+        dormitoryNum: "",
+        graduationYear: ""
+      },
+    };
+  },
+  methods: {
+    onFormSubmit(data) {
+      console.log("提交表单", data);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.appointment-page {
+  width: 100%;
+  height: 100%;
+  > .header {
+    width: 100%;
+    height: 100px;
+    line-height: 100px;
+    font-size: 24px;
+    color: #0bbd0b;
+    text-align: center;
+    border-bottom: 1px solid #ebedf0;
+  }
+  > .form {
+    width: 100%;
+    > .form-item {
+      width: 100%;
+      .form-label {
+        > span {
+          font-size: 20px;
+          font-weight: bold;
+        }
+      }
+    }
+    > .submit-btn {
+      margin-top: 20px;
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 const HomePage = () => import("@/pages/Home/home");
 const BindPage = () => import("@/pages/wechat-bind/wechat-bind");
+const AppointmentPage = () => import("@/pages/Appointment/appointment");
 
 const routes = [
+    { path: '/appointment', component: AppointmentPage, meta: { title: '预约送水' } },
     { path: '/bind', component: BindPage, meta: { title: '微信绑定' } },
     { path: '/home', redirect: '/', meta: { title: 'xxx水行' } },
     { path: '/', component: HomePage, meta: { title: 'xxx水行' } },
